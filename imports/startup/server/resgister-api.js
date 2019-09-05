ee1528d13c0e9825c928f4537199e38914b6ea45
Stuart Mitchell <stu@16pixels.com.au>
2019-09-05 19:28:38 +1000

    Add experiencetype enum

--- imports/startup/server/resgister-api.js
+++ imports/startup/server/resgister-api.js
@@ -11,7 +11,7 @@ import GuiesResolvers from "../../api/guides/resolvers";
 import QuerySchema from "../../api/query.graphql";
 import MutationSchema from "../../api/mutation.graphql";
 
-//hiii manyuiuty sdfffryytyuoopomiuypoipiepp0rre
+//hiii manyuiuty sdfffryytyuoopomiuypoipiepp0rrerupsuy
 
 const typeDefs = [ExperiencesSchema, GuidesSchema, QuerySchema, MutationSchema];
 
