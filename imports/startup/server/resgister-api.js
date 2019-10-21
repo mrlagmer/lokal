d7a4e66e489bd990c6afe749dcc033039a8d2cae
Stuart Mitchell <stu@16pixels.com.au>
2019-10-21 21:20:45 +1100

    Setup pre render.io

--- imports/startup/server/resgister-api.js
+++ imports/startup/server/resgister-api.js
@@ -2,6 +2,7 @@ import { ApolloServer, AuthenticationError, gql } from "apollo-server-express";
 import { WebApp } from "meteor/webapp";
 import { getUser } from "meteor/apollo";
 import merge from "lodash/merge";
+import prerenderIO from "prerender-node";
 
 import ExperiencesSchema from "../../api/experiences/experiences.graphql";
 import GuidesSchema from "../../api/guides/guides.graphql";
@@ -11,11 +12,15 @@ import GuiesResolvers from "../../api/guides/resolvers";
 import QuerySchema from "../../api/query.graphql";
 import MutationSchema from "../../api/mutation.graphql";
 
-//hiii manyuiuty sdfffryytyuoopomiuypoipiepp0rrerupsuyp
+//hiii manyuiuty sdfffryytyuoopomiuypoipiepp0rrerupsuyprto
+
+prerenderIO.set("prerenderToken", Meteor.settings.prerenderIo.prerenderToken);
 
 const typeDefs = [ExperiencesSchema, GuidesSchema, QuerySchema, MutationSchema];
 
 const resolvers = merge(ExperiencesResolvers, GuiesResolvers);
+prerenderIO.set("protocol", "https");
+WebApp.rawConnectHandlers.use(prerenderIO);
 
 const server = new ApolloServer({
   typeDefs,
